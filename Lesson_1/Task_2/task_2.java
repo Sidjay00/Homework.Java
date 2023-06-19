a619aa1f6f81e4bb15b147fc70c6c52b69a3a72d
SidorovK <2887458@gmail.com>
2023-06-19 20:08:27 +0300

    doing task 2

--- Lesson_1/Task_2/task_2.java
+++ Lesson_1/Task_2/task_2.java
@@ -1,7 +1,15 @@
 package Lesson_1.Task_2;
 
 public class task_2 {
-    public static void main() {
-        
+    public static void main(String[] args) {
+        int[] numbers = new int[1000];
+        int i = 0;
+        while (i < 1000) {
+            numbers[i] = 0 + i;
+            i++;
+        }
+
+
+        System.out.println(numbers[900]);
     }
 }
