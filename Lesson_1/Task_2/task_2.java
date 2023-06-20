d9cc6773d35db7b515cb922d318a3ea2c7f9edca
Sidjay00 <104720500+Sidjay00@users.noreply.github.com>
2023-06-20 16:41:31 +0300

    Update task_2.java
    added the solution

--- Lesson_1/Task_2/task_2.java
+++ Lesson_1/Task_2/task_2.java
@@ -8,8 +8,22 @@ public class task_2 {
             numbers[i] = 0 + i;
             i++;
         }
+        int num = 0;
+        String primeNumbers = "";
 
-
+        for (i = 1; i <= 1000; i++) {
+            int counter = 0;
+            for (num = i; num >= 1; num--) {
+                if (i % num == 0) {
+                    counter++;
+                }
+            }
+            if (counter == 2) {
+                primeNumbers = primeNumbers + i + " ";
+            }
+        }
+        System.out.println("Простые числа от 1 до 1000:");
+        System.out.println(primeNumbers);
         System.out.println(numbers[900]);
     }
 }
