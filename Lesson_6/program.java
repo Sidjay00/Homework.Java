3ac1b5ef00e1cfc2610a7a4660a32c3d93ef7439
SidorovK <2887458@gmail.com>
2023-07-10 23:19:38 +0300

    trying to solve the lesson 6 homework

--- Lesson_6/program.java
+++ Lesson_6/program.java
@@ -27,6 +27,54 @@ public class program {
         public void setManufacturer(String manufacturer) {
             this.manufacturer = manufacturer;
         }
-        
+        public String getModel() {
+            return model;
+        }
+        public void setModel(String model) {
+            this.model = model;
+        }
+        public int getPrice() {
+            return price;
+        }
+        public void setPrice(int price) {
+            this.price = price;
+        }
+        public int getQuantityInStock() {
+            return quantityInStock;
+        }
+        public void setQuantityInStock(int quantityInStock) {
+            this.quantityInStock = quantityInStock;
+        }
+        public double getScreenSize() {
+            return screenSize;
+        }
+        public void setScreenSize(double screenSize) {
+            this.screenSize = screenSize;
+        }
+        public int getRamSize() {
+            return ramSize;
+        }
+        public void setRamSize(int ramSize) {
+            this.ramSize = ramSize;
+        }
+        public int getHddSize() {
+            return hddSize;
+        }
+        public void setHddSize(int hddSize) {
+            this.hddSize = hddSize;
+        }
+
+        @Override
+        public String toString() {
+            return "Notebook{" + 
+                    "manufacturer='" + manufacturer + '\'' +
+                    ", model='" + model + '\'' +
+                    ", price='" + price +
+                    ", quantityInStock='" + quantityInStock +
+                    ", screenSize='" + screenSize +
+                    ", ramSize='" + ramSize +
+                    ", hddSize='" + hddSize +
+                    '}';
+        }
     }
 }
