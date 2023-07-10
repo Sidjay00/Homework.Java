cbb841ca3378f3d3c7286659503e3e32acb33d17
SidorovK <2887458@gmail.com>
2023-07-10 23:56:39 +0300

    added a structure, filled some notebooks. Its started to work correctly

--- Lesson_6/notebooks.java
+++ Lesson_6/notebooks.java
@@ -5,7 +5,11 @@ import Lesson_6.program.Notebook;
 public class notebooks {
     public static void main(String[] args) {
         ArrayList<Notebook> notebooks = new ArrayList<>();
-        notebooks.add(new Notebook("Apple", "MacBook Pro", 1999, 5, 15.9, 16, 512));
-
+        notebooks.add(new Notebook("Apple", "MacBook Pro", 199999, 5, 15.9, 16, 512));
+        notebooks.add(new Notebook("Dell", "XPS 13", 39999, 1, 14.2, 16, 1024));
+    
+    for (Notebook notebook : notebooks) {
+        System.out.println(notebook.getManufacturer() + " " + notebook.getModel() + ": " + notebook.getPrice());
     }
+}
 }
